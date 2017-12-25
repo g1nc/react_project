e82c2e139d63cf85f5a199a244ce6dcb42570f02
Oleg Globin <ginc.xyz@gmail.com>
2017-12-25 09:07:29 +0300

    fetch only suppliers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.where(role: 'supplier').all
     @users = @users.where(city_id: params[:city_id]) if params[:city_id]
   end
 end
