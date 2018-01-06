0046d8d2da73ddb58e6ff775248c411df6e7d4c2
Oleg Globin <ginc.xyz@gmail.com>
2018-01-06 22:10:03 +0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,26 @@ Product.delete_all
 User.delete_all
 City.delete_all
 
-5.times do |i|
-  city = City.create(name: Faker::Address.city)
-  5.times do |j|
-    name  = Faker::Name.name
-    email = "test#{i}#{j}@test.test"
-    user = User.create(name: name, email: email, password: 'test123', city: city)
-    2.times do
-      Address.create(user: user, value: Faker::Address.street_address)
-    end
-    4.times do |k|
-      Product.create(user: user, name: Faker::Lorem.word, price: (k + 1) * 100)
+products = %w[Эспрессо Американо Латте Чай]
+cities = [
+  {
+    name: 'Севастополь',
+    suppliers: ['Кафе "3 пескаря"', 'Кофе дизлайк', 'Starfax coffee']
+  },
+  {
+    name: 'Симферополь',
+    suppliers: ['Coffee enemy', 'Bakhchisaray coffee', 'Кафе "Люста"']
+  }
+]
+
+cities.each do |c|
+  city = City.create(name: c[:name])
+  c[:suppliers].each_with_index do |supplier, index|
+    email = Faker::Internet.email
+    user = User.create(name: supplier, email: email, password: 'test123', city: city)
+    Address.create(user: user, value: Faker::Address.street_address)
+    products.each do |product|
+      Product.create(user: user, name: product, price: 100)
     end
   end
 end
